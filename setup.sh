8671a5d12b1ba8e7da95259a341519437f7fa53b
Yale Zhang <yzzhang@google.com>
2021-10-04 00:04:34 -0700

    Internal change
    PiperOrigin-RevId: 400646012

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2020 Google LLC.
+# Copyright 2021 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,30 +13,131 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
+#!/usr/bin/env bash
 # TCRM Cloud Environment setup script.
 
 set -e
 
-VIRTUALENV_PATH=$HOME/"tcrm-venv"
+COMPOSER_ENV_NAME='tcrm-env'
 
-# Create virtual environment with python3
-if [[ ! -d "${VIRTUALENV_PATH}" ]]; then
-  virtualenv -p python3 "${VIRTUALENV_PATH}"
+SERVICE_ACCOUNT_NAME='tcrm-sa'
+SERVICE_ACCOUNT_ROLE='editor'
+
+APIS_TO_BE_ENABLED=(
+ 'bigquery-json.googleapis.com'
+ 'cloudapis.googleapis.com'
+ 'composer.googleapis.com'
+ 'googleads.googleapis.com'
+ 'storage-api.googleapis.com'
+)
+
+LOCAL_DAGS_FOLDER='src/'
+
+LOCATION=us-central1
+
+COMPOSER_PYPI_PACKAGES=(
+  'dataclasses'
+  'googleads'
+  'immutabledict'
+)
+
+# Handling command line arguments
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --project_id=*)
+      project_id="${1#*=}"
+      ;;
+    --composer_env_name=*)
+      composer_env_name="${1#*=}"
+      ;;
+    --local_dags_folder=*)
+      local_dags_folder="${1#*=}"
+      ;;
+  esac
+  shift
+done
+
+if [[ -z "$project_id" ]]; then
+  echo "--project_id is required."
+  exit 1
 fi
 
-# Activate virtual environment.
-source "$VIRTUALENV_PATH"/bin/activate
-
-# Install Python dependencies.
-pip install -r requirements.txt
+composer_env_name=${composer_env_name:-$COMPOSER_ENV_NAME}
+local_dags_folder=${local_dags_folder:-$LOCAL_DAGS_FOLDER}
 
 # Download TCRM Dependencies.
 git clone "https://github.com/google/gps_building_blocks.git"
 mkdir -p src/plugins/gps_building_blocks/cloud/utils
 cp -r gps_building_blocks/py/gps_building_blocks/cloud/utils/* src/plugins/gps_building_blocks/cloud/utils
 
-# Setup cloud environment.
-PYTHONPATH=src:gps_building_blocks/py:$PYTHONPATH
-export PYTHONPATH
-python cloud_env_setup.py "$@"
+# Create service account.
+gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
+  --project "$project_id" \
+  --display-name $SERVICE_ACCOUNT_NAME
+
+service_account_email="$SERVICE_ACCOUNT_NAME@$project_id.iam.gserviceaccount.com"
+
+gcloud iam service-accounts \
+  add-iam-policy-binding "$service_account_email" \
+  --project "$project_id" \
+  --member="serviceAccount:$service_account_email" \
+  --role="roles/$SERVICE_ACCOUNT_ROLE"
+
+# Enable required Cloud APIs.
+for i in "${APIS_TO_BE_ENABLED[@]}"
+do
+  echo "Enabling Cloud API: $i"
+  gcloud services enable "$i" --project "$project_id"
+done
+
+# Create Cloud Composer environment.
+gcloud composer environments create "$COMPOSER_ENV_NAME" \
+  --location=$LOCATION \
+  --project "$project_id"
+
+# Composer environment variables.
+env_python_path=PYTHONPATH='/home/airflow/gcs:/home/airflow/gcs/plugins'
+gcloud composer environments update "$composer_env_name" \
+  --location $LOCATION \
+  --update-env-variables=$env_python_path \
+  --project "$project_id"
+
+# Install required Python packages on Cloud Composer environment.
+if [[ -f pypi_package.txt ]]; then
+  rm pypi_package.txt
+  touch pypi_package.txt
+fi
+
+for i in "${COMPOSER_PYPI_PACKAGES[@]}"
+do
+  echo "$i" >> pypi_package.txt
+done
+
+env_python_path=PYTHONPATH='/home/airflow/gcs:/home/airflow/gcs/plugins'
+gcloud composer environments update "$composer_env_name" \
+  --location $LOCATION \
+  --update-pypi-packages-from-file=pypi_package.txt \
+  --project "$project_id"
+
+# Copy local DAGs and dependencies to Cloud Storage dag and plugins folders.
+echo "Copying dags..."
+dag_items="./src/dags/*"
+for f in $dag_items
+do
+  gcloud composer environments storage dags import \
+    --environment "$composer_env_name" \
+    --source="$f" \
+    --location=$LOCATION \
+    --project "$project_id"
+done
+
+echo "Copying plugins..."
+plugin_items="./src/plugins/*"
+for f in $plugin_items
+do
+  gcloud composer environments storage plugins import \
+    --environment "$composer_env_name" \
+    --source="$f" \
+    --location=$LOCATION \
+    --project "$project_id"
+done
