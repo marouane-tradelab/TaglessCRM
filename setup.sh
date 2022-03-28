5d73e7d02b506227a7d7f46335eeb7324fa212a6
Yale Zhang <yzzhang@google.com>
2022-03-27 19:34:14 -0700

    Internal change
    PiperOrigin-RevId: 437651041

--- setup.sh
+++ setup.sh
@@ -18,10 +18,9 @@
 
 set -e
 
-COMPOSER_ENV_NAME='tcrm-env'
-
-SERVICE_ACCOUNT_NAME='tcrm-sa'
-SERVICE_ACCOUNT_ROLE='editor'
+DEFAULT_COMPOSER_ENV_NAME='tcrm-env'
+DEFAULT_LOCAL_DAGS_FOLDER='src/'
+DEFAULT_LOCATION=us-central1
 
 APIS_TO_BE_ENABLED=(
  'bigquery-json.googleapis.com'
@@ -31,13 +30,8 @@ APIS_TO_BE_ENABLED=(
  'storage-api.googleapis.com'
 )
 
-LOCAL_DAGS_FOLDER='src/'
-
-LOCATION=us-central1
-
 COMPOSER_PYPI_PACKAGES=(
   'dataclasses'
-  'googleads'
   'immutabledict'
 )
 
@@ -53,6 +47,9 @@ while [ $# -gt 0 ]; do
     --local_dags_folder=*)
       local_dags_folder="${1#*=}"
       ;;
+    --location=*)
+      location="${1#*=}"
+      ;;
   esac
   shift
 done
@@ -62,26 +59,24 @@ if [[ -z "$project_id" ]]; then
   exit 1
 fi
 
-composer_env_name=${composer_env_name:-$COMPOSER_ENV_NAME}
-local_dags_folder=${local_dags_folder:-$LOCAL_DAGS_FOLDER}
+composer_env_name=${composer_env_name:-$DEFAULT_COMPOSER_ENV_NAME}
+local_dags_folder=${local_dags_folder:-$DEFAULT_LOCAL_DAGS_FOLDER}
+location=${location:-$DEFAULT_LOCATION}
 
 # Download TCRM Dependencies.
+if [ -d "./gps_building_blocks" ]
+then
+    rm -rf "./gps_building_blocks"
+fi
+
+if [ -d "./src/plugins/gps_building_blocks" ]
+then
+    rm -rf "./src/plugins/gps_building_blocks"
+fi
+
 git clone "https://github.com/google/gps_building_blocks.git"
 mkdir -p src/plugins/gps_building_blocks/cloud/utils
 cp -r gps_building_blocks/py/gps_building_blocks/cloud/utils/* src/plugins/gps_building_blocks/cloud/utils
-
-# Create service account.
-gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
-  --project "$project_id" \
-  --display-name $SERVICE_ACCOUNT_NAME
-
-service_account_email="$SERVICE_ACCOUNT_NAME@$project_id.iam.gserviceaccount.com"
-
-gcloud iam service-accounts \
-  add-iam-policy-binding "$service_account_email" \
-  --project "$project_id" \
-  --member="serviceAccount:$service_account_email" \
-  --role="roles/$SERVICE_ACCOUNT_ROLE"
 
 # Enable required Cloud APIs.
 for i in "${APIS_TO_BE_ENABLED[@]}"
@@ -91,14 +86,14 @@ do
 done
 
 # Create Cloud Composer environment.
-gcloud composer environments create "$COMPOSER_ENV_NAME" \
-  --location=$LOCATION \
+gcloud composer environments create "$composer_env_name" \
+  --location=$location \
   --project "$project_id"
 
 # Composer environment variables.
 env_python_path=PYTHONPATH='/home/airflow/gcs:/home/airflow/gcs/plugins'
 gcloud composer environments update "$composer_env_name" \
-  --location $LOCATION \
+  --location $location \
   --update-env-variables=$env_python_path \
   --project "$project_id"
 
@@ -115,9 +110,15 @@ done
 
 env_python_path=PYTHONPATH='/home/airflow/gcs:/home/airflow/gcs/plugins'
 gcloud composer environments update "$composer_env_name" \
-  --location $LOCATION \
+  --location $location \
   --update-pypi-packages-from-file=pypi_package.txt \
   --project "$project_id"
+
+# Override airflow configuration
+gcloud composer environments update "$composer_env_name" \
+  --location $location \
+  --project "$project_id" \
+  --update-airflow-configs=scheduler-catchup_by_default=False
 
 # Copy local DAGs and dependencies to Cloud Storage dag and plugins folders.
 echo "Copying dags..."
@@ -127,7 +128,7 @@ do
   gcloud composer environments storage dags import \
     --environment "$composer_env_name" \
     --source="$f" \
-    --location=$LOCATION \
+    --location=$location \
     --project "$project_id"
 done
 
@@ -138,6 +139,6 @@ do
   gcloud composer environments storage plugins import \
     --environment "$composer_env_name" \
     --source="$f" \
-    --location=$LOCATION \
+    --location=$location \
     --project "$project_id"
 done
